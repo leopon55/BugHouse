08e3fb84d36435a2ca6db4f261c36b815c8958eb
leopon55 <re.o.5123re.o@gmail.com>
2020-06-27 03:49:04 +0900

    カウントアプリと、ビルド時のバグをfix

--- app/src/main/java/app/nickname/myoji/bughouse/CountActivity.kt
+++ app/src/main/java/app/nickname/myoji/bughouse/CountActivity.kt
@@ -6,7 +6,7 @@ import kotlinx.android.synthetic.main.activity_count.*
 
 class CountActivity : AppCompatActivity() {
     var number: Int = 0
-
+//    コミットできない
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_count)
